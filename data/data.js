7f24a479676d1a4a99b939e5b03c264fcc271220
RABrL <robin_brito@hotmail.com>
2023-02-08 00:46:55 -0500

    improve fetching data

--- data/data.js
+++ data/data.js
@@ -5,12 +5,43 @@ import Pokemon from '../models/Pokemon.js'
    * @param {number} id Id of pokemon
    * @returns
    */
-export function getPokemon (id) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+export function getPokemon (name) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then(res => res.json())
     .then(data => {
-      const { sprites, name, height, id, weight, types, stats, abilities } = data
-      const { other: { 'official-artwork': { front_default: img } } } = sprites
-      return new Pokemon(id, name, height, weight, types, stats, abilities, img)
+      const cleanedData = cleanData(data)
+      const { id, name, height, weight, nameTypes: types, cleanedStats: stats, nameAbilities: abilities, img } = cleanedData
+      const pokemon = new Pokemon(id, name, height, weight, types, stats, abilities, img)
+      pokemon.setActualPokemon(name)
+      return pokemon
     })
+}
+
+function cleanData (data) {
+  const { sprites, name, height, id, weight, types, stats, abilities } = data
+  const { other: { 'official-artwork': { front_default: img } } } = sprites
+  const nameTypes = types.map(({ type: { name } }) => name)
+  const cleanedStats = stats.map(({ base_stat: value, stat: { name } }) => {
+    return {
+      name,
+      value
+    }
+  })
+  const nameAbilities = abilities.map(({ ability: { name } }) => name)
+  return {
+    id,
+    name,
+    height,
+    weight,
+    nameTypes,
+    cleanedStats,
+    nameAbilities,
+    img
+  }
+}
+
+export async function getAllPokemons () {
+  return await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1279')
+    .then(res => res.json())
+    .then(data => data.results.map(({ name }) => name))
 }
